56ef03e7a3a768712c71f4aa84bc6c021a755568
Dylan <dylan496517985@gmail.com>
2017-08-25 12:39:41 +1000

    finish file upload and delete

---
+++ fossickpointToolbox/toolbox/static/toolbox/js/content.js
@@ -0,0 +1,100 @@
+var csrf_token = getCookie('csrftoken');
+
+function getCookie(name) {
+    var cookieValue = null;
+    if (document.cookie && document.cookie !== '') {
+        var cookies = document.cookie.split(';');
+        for (var i = 0; i < cookies.length; i++) {
+            var cookie = jQuery.trim(cookies[i]);
+            // Does this cookie string begin with the name we want?
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookieValue;
+}
+function csrfSafeMethod(method) {
+    // these HTTP methods do not require CSRF protection
+    return (/^(GET|HEAD|OPTIONS|TRACE)$/.test(method));
+}
+$.ajaxSetup({
+    beforeSend: function(xhr, settings) {
+        if (!csrfSafeMethod(settings.type) && !this.crossDomain) {
+            xhr.setRequestHeader("X-CSRFToken", csrf_token);
+        }
+    }
+});
+
+function deleteContent(id)
+{
+    $.post('./content',{'operation':'delete','id':id},function (result){
+        if (result["status"] == 1)
+        {
+            alert("delete successfully");
+            window.location.href='../content';
+        }
+
+
+    })
+}
+function clearUploadForm() {
+    $("#title").val("");
+    $("#tag").val("");
+    $("keyword").val("");
+}
+$(document).ready(function(){
+    //打开弹出窗口
+    //按钮点击事件!
+    $("#add").click(function(){
+        //调用函数居中窗口
+          layer.open({
+            type: 1,
+            area: ['600px', '700px'],
+            title: 'Add content',
+            shadeClose: false, //点击遮罩关闭
+            content: $('#addContent')
+          });
+    });
+
+    $("#upload").click(function(){
+        var fileobj = $("#doc-form-file")[0].files[0];
+        var form = new FormData();
+        var csrf_token = getCookie('csrftoken');
+//        form.append('operation','add');
+        form.append('operation','add'),
+        form.append('file',fileobj);
+        form.append('title', $("#title").val());
+        form.append('type',$("#selectedType").val());
+       form.append('tag',$("#tag").val());
+        form.append('keyword',$("#keyword").val());
+        $.ajax({
+            type:'POST',
+            url:'./content',
+            data:form,
+            processData:false,  // 告诉jquery不转换数据
+            contentType:false,  // 告诉jquery不设置内容格式
+
+            success:function (arg) {
+                if (arg["status"] == "1")
+                {
+                    alert("upload file successfully");
+                    window.location.href='../content';
+                }
+                else
+                {
+                    alert("upload file failed, please finish the form");
+                    clearUploadForm();
+                }
+            }
+        })
+
+        $("#reset").click(function(){
+            clearUploadForm();
+        });
+    });
+
+
+
+});
